54a95f83b580f2048962cb54b33d1637ef27efb7
Tamanna-2004 <tamannaaggarwal168@gmail.com>
2025-01-23 17:27:43 +0530

    Update Card2.jsx

--- src/component/Card2.jsx
+++ src/component/Card2.jsx
@@ -5,7 +5,7 @@ const Card = (props) => {
   return (
     <div className="card_div flex flex-col gap-4 p-6 border rounded-3xl min-w-[50%]
      md:w-[30%] md:min-w-[25%] md:gap-6 duration-[1500ms] ease-in-out hover:bg-custom-gradient-hover hover:scale-75">
-      <h1 className="text-4xl text-purple-200 animate__animated animate__pulse ">{props.heading}</h1>
+      <h1 className="text-xl text-purple-200 animate__animated animate__pulse md:text-4xl ">{props.heading}</h1>
       <div className="grid gap-2 grid-cols-3 md:grid-cols-3">
         <img src={props.img1} alt="react" className="sm:w-16 sm:h-16 rounded-full"/>
         <img src={props.img2} alt="tailwind" className="sm:w-16 sm:h-16" />
